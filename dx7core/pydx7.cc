da54532b014a28d78355096b984ee6bc4945a7c7
Brian Whitman <brian@variogr.am>
2018-08-27 09:45:04 -0400

    better defaults

--- dx7core/pydx7.cc
+++ dx7core/pydx7.cc
@@ -72,13 +72,19 @@ void init_synth(void) {
 static PyObject * render_wrapper(PyObject *self, PyObject *args) {
   int arg1, arg2, arg3, arg4, arg5;
 
-  arg1 = 8; /* Default values. */
+  /* Default values. */
+  arg1 = 8; // patch #
+  arg2 = 50; // midi note
+  arg3 = 70; // velocity
+  arg4 = 44100; // samples
+  arg5 = 22050; // keyup sample
   if (! PyArg_ParseTuple(args, "iiiii", &arg1, &arg2, &arg3, &arg4, &arg5)) {
     return NULL;
   }
   short * result;
   result = render(arg1, arg2, arg3, arg4, arg5);
 
+  // Create a python list of ints (they are signed shorts that come back)
   PyObject* ret = PyList_New(arg4); // arg4 is samples
   for (int i = 0; i < arg4; ++i) {
     PyObject* python_int = Py_BuildValue("i", result[i]);
@@ -98,20 +104,24 @@ extern "C" {
 DL_EXPORT(void) initdx7(void)
 {
   Py_InitModule("dx7", DX7Methods);
-
+  // TODO: this file should go into the package directory from setup.py, but that is a PITA 
   FILE *f = fopen("/Users/bwhitman/outside/learnfm/compact.bin","rb");
+  // See how many voices are in the file
   fseek(f, 0, SEEK_END);
   long fsize = ftell(f);
   char * all_patches = (char*)malloc(fsize);
   fseek(f,0,SEEK_SET);
+  // Load them all in
   fread(all_patches, 1, fsize, f);
   fclose(f);
   int patches = fsize / 128;
   printf("%d patches\n", patches);
+  // Patches have to be unpacked to go from 128 bytes to 156 bytes via DX7 spec
   unpacked_patches = (char*) malloc(patches*156);
   for(int i=0;i<patches;i++) {
     UnpackPatch(all_patches + (i*128), unpacked_patches + (i*156));
   }
+  // Have to call this out as we're in C extern 
   init_synth();
   free(all_patches);
 }
